240d71f56de8bb0aee9c0d672eb9354b2f235180
mdraselmiah95 <russelroy15@gmail.com>
2022-07-16 22:15:16 +0600

    New project added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: ["14px"],
+      },
+      colors: {
+        starGreen: "#D4E9E2",
+        darkGreen: "#1E3932",
+        lightText: "rgba(0,0,0, .58)",
+        titleText: "rgba(0,0,0, .87)",
+      },
+    },
+  },
+  plugins: [],
+};
